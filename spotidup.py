867771f8e591c646d3b6c56b08e40510c3d3f0cb
Asbjørn Ottesen Steinskog <asbjorn@steinskog.me>
2013-05-21 00:16:38 +0200

    Add count for distinct songs

--- spotidup.py
+++ spotidup.py
@@ -7,7 +7,7 @@ from collections import Counter
 
 def makelist(f = open('songs.txt', 'r')):
 	a = [line for line in f]
-	return Counter(a) - Counter(set(a))
+	return Counter(a) - Counter(set(a)),len(Counter(set(a)))
 
 def urltitle(url):
    f = urlopen(url).read()
@@ -21,12 +21,14 @@ def charfix(string):
 		string = sub(k,chars[k],string)
 	return string
 
-titles = [urltitle(track) for track in makelist()]
+l=makelist()
+
+titles = [urltitle(track) for track in l[0]]
 
 if len(titles)==0:
-	print('No duplicates')
+	print('The playlist contains ' + str(l[1]) + ' songs and no duplicates.')
 else:
 	with open("duplicates.txt", "w") as dup:
-		dup.write('Found ' + str(len(titles)) + ' duplicates: \n\n')
+		dup.write('Your playlist contains ' + str(l[1]) + ' distinct songs.\nFound ' + str(len(titles)) + ' duplicates: \n\n')
 		[dup.write(charfix(titles[i])+'\n') for i in range(0, len(titles))]
-	print('Done. Check duplicates.txt for duplicates.')
+	print('Found ' + str(l[1]) + ' distinct songs and ' + str(len(titles)) + ' duplicates. Check duplicates.txt for a list of the dupliates.')
